8b4347901d8bcd665969bf750a7fb19d989607b0
Orsouene <orsoueneelaouizeb@gmail.com>
2025-02-11 17:47:37 +0100

    -Aggiunto dei  alert di errore . nell Componente  di ContactHost e nella pagina di Property Detail Page -Modifica del layout di Property Detail Page

---
+++ src/components/ContactHost.jsx
@@ -0,0 +1,208 @@
+import React, { useState } from "react";
+import style from "../components/ContactHost.module.css";
+
+const newPost = {
+  nome: "",
+  email: "",
+  message: "",
+};
+
+function PaginaContact() {
+  const [messageSent, setMessageSent] = useState(false);
+  const [formData, setFormData] = useState(newPost);
+  const [formError, setFormError] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    // Controllo del nome
+    if (!formData.nome.trim()) {
+      setFormError("Per favore inserisci un nome");
+      setTimeout(() => setFormError(""), 2000);
+      return;
+    }
+
+    // Controllo dell'email
+    const email = formData.email;
+    if (!email || email.trim() === "") {
+      setFormError("Per favore inserisci una email");
+      return;
+    }
+    if (email.indexOf("@") === -1 || email.indexOf(".") === -1) {
+      setFormError("Per favore inserisci una email valida");
+      setTimeout(() => setFormError(""), 2000);
+      return;
+    }
+    const [localPart, domainPart] = email.split("@");
+    if (
+      !localPart ||
+      !domainPart ||
+      localPart.trim() === "" ||
+      domainPart.trim() === ""
+    ) {
+      setFormError("Per favore inserisci una email valida");
+      setTimeout(() => setFormError(""), 2000);
+      return;
+    }
+    if (domainPart.indexOf(".") === -1) {
+      setFormError("Per favore inserisci una email valida");
+      setTimeout(() => setFormError(""), 2000);
+      return;
+    }
+    const [domainName, extension] = domainPart.split(".");
+    if (
+      !domainName ||
+      !extension ||
+      domainName.trim() === "" ||
+      extension.trim() === ""
+    ) {
+      setFormError("Per favore inserisci una email valida");
+      setTimeout(() => setFormError(""), 2000);
+      return;
+    }
+
+    // Controllo del messaggio
+    if (!formData.message.trim()) {
+      setFormError("Per favore inserisci un messaggio");
+      setTimeout(() => setFormError(""), 2000);
+      return;
+    }
+
+    setFormError("");
+    setMessageSent(true);
+    setTimeout(() => {
+      setMessageSent(false);
+    }, 2000);
+    setFormData(newPost); // Resetta il form dopo l'invio
+  };
+
+  function handleChange(e) {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  }
+
+  const handleInvalid = (e) => {
+    e.preventDefault();
+    if (!formError) {
+      setFormError("Per favore completa tutti i campi.");
+    }
+  };
+
+  return (
+    <div className={style.formCard1}>
+      <div className={style.formCard2}>
+        <form
+          className={style.form}
+          onSubmit={handleSubmit}
+          noValidate
+          onInvalid={handleInvalid}
+        >
+          <div className="m-auto mt-2  p-2">
+            <p style={{ fontSize: "1.5rem" }}>Contattaci </p>
+          </div>
+
+          <div className={style.formField}>
+            <input
+              required
+              placeholder="Nome"
+              className={style.inputField}
+              type="text"
+              name="nome"
+              value={formData.nome}
+              onChange={handleChange}
+            />
+          </div>
+
+          <div className={style.formField}>
+            <input
+              required
+              placeholder="Email"
+              className={style.inputField}
+              type="email"
+              name="email"
+              value={formData.email}
+              onChange={handleChange}
+            />
+          </div>
+
+          <div className={style.formField}>
+            <textarea
+              required
+              placeholder="Messaggio"
+              cols="30"
+              rows="3"
+              className={style.inputField}
+              name="message"
+              value={formData.message}
+              onChange={handleChange}
+            ></textarea>
+          </div>
+
+          <button type="submit" className={style.sendMessageBtn}>
+            Invia Messaggio
+          </button>
+          {/* messagio di errore  */}
+          {formError && (
+            <div className="text-red-500 mt-4">
+              <div className="relative w-full m-auto max-w-80 flex flex-wrap items-center justify-center py-1 pl-4 pr-14 rounded-lg text-base font-medium [transition:all_0.5s_ease] border-solid border border-[#f85149] text-[#b22b2b] [&_svg]:text-[#b22b2b] group bg-[linear-gradient(#f851491a,#f851491a)]">
+                <p className="flex flex-row items-center mr-auto gap-x-2">
+                  <svg
+                    stroke="currentColor"
+                    fill="none"
+                    stroke-width="2"
+                    viewBox="0 0 24 24"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    height="28"
+                    width="28"
+                    className="h-7 w-7"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path d="m21.73 18-8-14a2 2 0 0 0-3.48 0l-8 14A2 2 0 0 0 4 21h16a2 2 0 0 0 1.73-3Z"></path>
+                    <path d="M12 9v4"></path>
+                    <path d="M12 17h.01"></path>
+                  </svg>
+                  {formError}
+                </p>
+              </div>
+            </div>
+          )}
+          {/* Messaggio di conferma */}
+          {messageSent && (
+            <section>
+              <div className="space-y-2 p-4">
+                <div
+                  role="alert"
+                  className="bg-green-100 dark:bg-green-900 border-l-4 border-green-500 dark:border-green-700 text-green-900 dark:text-green-100 p-2 rounded-lg flex items-center transition duration-300 ease-in-out hover:bg-green-200 dark:hover:bg-green-800 transform hover:scale-105"
+                >
+                  <svg
+                    stroke="currentColor"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    className="h-5 w-5 flex-shrink-0 mr-2 text-green-600"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      d="M13 16h-1v-4h1m0-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+                      strokeWidth="2"
+                      strokeLinejoin="round"
+                      strokeLinecap="round"
+                    ></path>
+                  </svg>
+                  <p className="text-xs font-semibold">
+                    Messaggio inviato con successo!
+                  </p>
+                </div>
+              </div>
+            </section>
+          )}
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default PaginaContact;
