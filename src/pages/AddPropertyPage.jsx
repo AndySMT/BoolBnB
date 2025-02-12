711c69139e08499ae9a8c65d3fa3d518fcb33dd9
Aj-Herrera-99 <ajherrera999@gmail.com>
2025-02-12 10:56:11 +0100

    overlay sotto il form

--- src/pages/AddPropertyPage.jsx
+++ src/pages/AddPropertyPage.jsx
@@ -179,7 +179,7 @@ function InfoSection() {
 
 function FormSection({ setIsOpen, children }) {
     return (
-        <section className="fixed inset-0 flex items-center justify-center z-50 bg-gray-500 opacity-100">
+        <section className="fixed inset-0 flex items-center justify-center z-50 bg-black/80">
             <div className="bg-white p-6 rounded-lg shadow-xl w-[600px] relative animate-fade-in">
                 <button
                     className="absolute top-2 right-2 text-gray-500 hover:text-gray-700"
