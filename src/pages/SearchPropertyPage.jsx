87391f3cf31aee59880ae39a25e45cb68e9b726f
Aj-Herrera-99 <ajherrera999@gmail.com>
2025-02-15 00:15:34 +0100

    logica filtro avanzato "completato", refactoring quasi, manca react query

--- src/pages/SearchPropertyPage.jsx
+++ src/pages/SearchPropertyPage.jsx
@@ -1,145 +1,203 @@
-import React from 'react'
-import { useState } from 'react';
-import { useGetPropertiesQuery } from "../hooks/useDataQuery";
-import CardsSection from "../components/CardsSection";
-import Card from "../components/Card";
-// import { useRef, useEffect } from 'react'
+import React, { useEffect } from "react";
+import { useState } from "react";
+import Select from "react-select";
+import axios from "axios";
+import { baseUrl, propsEndpoint } from "../globals/apiUrls";
 
+// options delle select iniziali
+const initialOptions = {
+    n_bathrooms: 0,
+    n_beds: 0,
+    square_meters: 0,
+    n_bedrooms: 0,
+    property_type: "",
+};
+// todo: refactor con react query
 function SearchPropertyPage() {
-  //! Stato per il loading, l'errore e i dati
-  const { isLoading, isError, data } = useGetPropertiesQuery();
-  //! Stato per i filtri
-  const [filter, setFilter] = useState(null);
-  //! Utilizza useState per tracciare lo stato "active"
-  const [isActive, setIsActive] = useState(false);
+    const [inputValue, setInputValue] = useState(""); // input per ricerca tramite luogo
+    const [properties, setProperties] = useState([]); // per salvare risposta proprieta dal server
+    // oggetto che salva le options
+    const [optSelected, setOptSelected] = useState(initialOptions);
+    // filtro in base a input di ricerca luogo
+    const filteredProperties = properties?.filter((prop) =>
+        prop?.city?.toLowerCase().includes(inputValue.toLowerCase())
+    );
 
-  if (isLoading) return <div>Loading...</div>;
-  if (isError) return <pre>Error</pre>;
+    // * ACTIONS
+    const handleInputChange = (e) => {
+        setInputValue(e.target.value);
+    };
+    
+    const onSubmit = (e) => {
+        e.preventDefault();
+        const params = optSelected; // le options le indico come params da mettere nella query params
+        axios.get(baseUrl + propsEndpoint, { params }).then((res) => {
+            setProperties(res.data);
+            // al success, reset delle opzioni delle selects
+            setOptSelected(initialOptions);
+        });
+    };
 
+    // * QUERIES
+    useEffect(() => {
+        // fetch iniziale delle proprieta
+        axios
+            .get(baseUrl + propsEndpoint)
+            .then((res) => setProperties(res.data))
+            .catch((err) => console.error(err));
+    }, []);
 
-  //! Funzione per gestire l'evento di focus sull'input
-  const handleFocus = () => {
-    setIsActive(true); //? Attiva lo stato quando l'input ha focus
-  };
-
-  //! Funzione per gestire l'evento di blur ovvero quando perde il focus
-  const handleBlur = () => {
-    setIsActive(false);  //? Disattiva lo stato quando l'input perde il focus
-  };
-
-  const filteredProperties = !filter ? data : data.filter(prop => prop.property_type?.toLowerCase().includes(filter.toLowerCase()));
-
-
-  return (
-    <>
-      <form
-        className='flex flex-col md:flex-row mt-5 mx-10 bg-linear-90/oklch sm:drop-shadow-lg from-15% from-[#cccccc] to-[#b7b7b7] justify-around gap-x-8 gap-y-4 px-5 py-4 [&_label]:text-sm [&_input]:bg-[#e2e2e2] [&_input]:rounded-3xl rounded-2xl [&_input]:shadow-[0_7px_7px_rgba(0,0,0,0.25)] inset-shadow-[7px_7px_7px_rgba(0,0,0,0.25)] [&_input]:focus:outline-none'
-      >
-        {/* Place section */}
-        <div>
-          <label htmlFor="place">Luogo</label>
-          <input type="text" name="place" id="place" className='h-12 p-3 w-full' onFocus={handleFocus}
-            onBlur={handleBlur} />
-        </div>
-        {/* # mq and property_type section */}
-        <div className='grid grid-cols-2 gap-4'>
-          {/* property_type section */}
-          <div>
-            <label htmlFor="propriety_type">Tipo di proprietà</label>
-            <select name='propriety_type' id='propriety_type' className='h-12 p-3 w-full bg-[#e2e2e2] rounded-3xl shadow-[0_7px_7px_rgba(0,0,0,0.25)] focus:outline-none' value={filter.data}>
-              <option value="prop">Scegli la proprietà</option>
-              <option value="villa">Villa</option>
-              <option value="appartamento">Appartamento</option>
-              <option value="chalet">Chalet</option>
-              <option value="baita">Baita</option>
-              <option value="casa_indipendente">Casa indipendente</option>
-              <option value="villa_a_schiera">Villa a schiera</option>
-            </select>
-          </div>
-          {/* mq section */}
-          <div>
-            <label htmlFor="surface">Metri quadrati</label>
-            <select name='surface' id='surface' className='h-12 p-3 w-full bg-[#e2e2e2] rounded-3xl shadow-[0_7px_7px_rgba(0,0,0,0.25)] focus:outline-none'>
-              <option value="metri">Metri quadrati</option>
-              <option value="0">0+</option>
-              <option value="50">50+</option>
-              <option value="100">100+</option>
-              <option value="150">150+</option>
-              <option value="200">200+</option>
-              <option value="250">250+</option>
-            </select>
-          </div>
-        </div>
-
-        {/* #rooms */}
-        <div className='grid grid-cols-3 gap-4'>
-          {/* #bedrooms Section */}
-          <div>
-            <label htmlFor="bedrooms"># camere</label>
-            <select name='bedrooms' id='bedrooms' className='h-12 p-3 w-full bg-[#e2e2e2] rounded-3xl shadow-[0_7px_7px_rgba(0,0,0,0.25)] focus:outline-none'>
-              <option value="camere"># camere</option>
-              <option value="1">1+</option>
-              <option value="2">2+</option>
-              <option value="3">3+</option>
-              <option value="4">4+</option>
-              <option value="5">5+</option>
-            </select>
-          </div>
-          {/* #bathrooms section */}
-          <div>
-            <label htmlFor="bathrooms"># bagni</label>
-            <select name='bathrooms' id='bathrooms' className='h-12 p-3 w-full bg-[#e2e2e2] rounded-3xl shadow-[0_7px_7px_rgba(0,0,0,0.25)] focus:outline-none'>
-              <option value="camere"># bagni</option>
-              <option value="1">1+</option>
-              <option value="2">2+</option>
-              <option value="3">3+</option>
-              <option value="4">4+</option>
-            </select>
-          </div>
-          <div className='relative'>
-            <input type="submit" value="Invia" className='p-3 absolute left-0 bottom-0' />
-          </div>
-        </div>
-      </form>
-
-      {isActive && (
-        <div className='bg-radial-[at_25%_25%] from-blue-200 to-blue-300 to-75% mx-10 my-5 md:my-5 py-5 rounded-2xl inset-shadow-[7px_7px_7px_rgba(0,0,0,0.25)]'>
-          <h2 className='text-center'>Dove vuoi andare</h2>
-          <div className='flex gap-4 overflow-scroll mx-5 [&_h2]:text-sm [&_div]:shrink-0 [&_div]:w-40 [&_img]:rounded-xl [&_img]:aspect-square'>
-            <div>
-              <img src="./regions/flexibility.jpg" alt="earth" />
-              <h2>Sono flessibile</h2>
+    // * RETURNS
+    return (
+        <>
+            <div className="grid grid-cols-2">
+                <div>
+                    <input
+                        type="text"
+                        className="border rounded-lg px-4 py-2 block mx-auto"
+                        placeholder="Inserisci città"
+                        value={inputValue}
+                        onChange={handleInputChange}
+                    />
+                    <form
+                        onSubmit={onSubmit}
+                        className="mx-auto my-4 w-1/2 bg-amber-200 min-h-[60vh]"
+                    >
+                        <Selects setOptSelected={setOptSelected} />
+                        {/* submit */}
+                        <button
+                            type="submit"
+                            className="px-4 py-2 bg-amber-500 rounded-lg"
+                        >
+                            Cerca
+                        </button>
+                    </form>
+                </div>
+                <Countries />
             </div>
-            <div>
-              <img src="./regions/africa.jpg" alt="africa" />
-              <h2>Africa</h2>
+            {/* data */}
+            <div className="bg-green-300 text-center min-h-[200px]">
+                {properties.length && inputValue.length ? (
+                    filteredProperties.map((prop) => (
+                        <div key={prop.id}>{prop.title}</div>
+                    ))
+                ) : (
+                    <p>Nessun risultato ancora</p>
+                )}
             </div>
-            <div>
-              <img src="./regions/spagna.jpg" alt="spagna" />
-              <h2>Spagna</h2>
-            </div>
-            <div>
-              <img src="./regions/sudamerica.jpg" alt="sudamerica" />
-              <h2>Sud America</h2>
-            </div>
-            <div>
-              <img src="./regions/sudestasia.jpg" alt="sudestasia" />
-              <h2>Sud-est Asia</h2>
-            </div>
-            <div>
-              <img src="./regions/usa.jpg" alt="usa" />
-              <h2>USA</h2>
-            </div>
-          </div>
-        </div>
-      )}
-      <CardsSection title={""}>
-        {filteredProperties?.map((prop, index) => (
-          <Card key={prop.id} index={index} property={prop} />
-        ))}
-      </CardsSection>
-    </>
-  )
+        </>
+    );
 }
 
-export default SearchPropertyPage
+const options = [
+    { value: "1", label: "1+" },
+    { value: "2", label: "2+" },
+    { value: "3", label: "3+" },
+    { value: "4", label: "4+" },
+    { value: "5", label: "5+" },
+];
+
+const smqOptions = [
+    { value: "50", label: "50+" },
+    { value: "100", label: "100+" },
+    { value: "200", label: "200+" },
+    { value: "300", label: "300+" },
+    { value: "400", label: "400+" },
+];
+
+const typeOptions = [
+    { value: "villa", label: "Villa" },
+    { value: "appartamento", label: "Appartamento" },
+    { value: "chalet", label: "Chalet" },
+    { value: "baita", label: "Baita" },
+    { value: "casa_indipendente", label: "Casa indipendente" },
+    { value: "villa_a_schiera", label: "Villa a schiera" },
+];
+
+const customStyles = {
+    control: () => {}, //style della select
+    option: () => {}, //style delle options
+};
+
+const selectFields = [
+    {
+        label: "Camere da letto",
+        id: "camere_letto",
+        field: "n_bedrooms",
+        options: options,
+    },
+    { label: "Letti", id: "letti", field: "n_beds", options: options },
+    { label: "Bagni", id: "bagni", field: "n_bathrooms", options: options },
+    {
+        label: "Metri quadri",
+        id: "smq",
+        field: "square_meters",
+        options: smqOptions,
+    },
+    {
+        label: "Tipo di proprietà",
+        id: "p_type",
+        field: "property_type",
+        options: typeOptions,
+    },
+];
+
+const Selects = ({ setOptSelected }) => {
+    return (
+        <>
+            {selectFields.map(({ label, id, field, options }) => (
+                <div key={id}>
+                    <label htmlFor={id}>{label}</label>
+                    <Select
+                        id={id}
+                        name={id}
+                        options={options}
+                        onChange={(opt) =>
+                            setOptSelected((prev) => ({
+                                ...prev,
+                                [field]: opt.value,
+                            }))
+                        }
+                    />
+                </div>
+            ))}
+        </>
+    );
+};
+
+function Countries() {
+    return (
+        <div className="bg-radial-[at_25%_25%] from-blue-200 to-blue-300 to-75% mx-10 my-5 md:my-5 py-5 rounded-2xl inset-shadow-[7px_7px_7px_rgba(0,0,0,0.25)]">
+            <h2 className="text-center">Dove vuoi andare</h2>{" "}
+            <div className="flex gap-4 overflow-scroll mx-5 [&_h2]:text-sm [&_div]:shrink-0 [&_div]:w-40 [&_img]:rounded-xl [&_img]:aspect-square">
+                {" "}
+                <div>
+                    <img src="./regions/flexibility.jpg" alt="earth" />
+                    <h2>Sono flessibile</h2>{" "}
+                </div>{" "}
+                <div>
+                    <img src="./regions/africa.jpg" alt="africa" />
+                    <h2>Africa</h2>{" "}
+                </div>{" "}
+                <div>
+                    <img src="./regions/spagna.jpg" alt="spagna" />
+                    <h2>Spagna</h2>{" "}
+                </div>{" "}
+                <div>
+                    <img src="./regions/sudamerica.jpg" alt="sudamerica" />
+                    <h2>Sud America</h2>{" "}
+                </div>{" "}
+                <div>
+                    <img src="./regions/sudestasia.jpg" alt="sudestasia" />
+                    <h2>Sud-est Asia</h2>{" "}
+                </div>{" "}
+                <div>
+                    <img src="./regions/usa.jpg" alt="usa" />
+                    <h2>USA</h2>{" "}
+                </div>{" "}
+            </div>{" "}
+        </div>
+    );
+}
+
+export default SearchPropertyPage;
