3083aa694de6fc8d230e0732c32028e61d3a53c4
Orsouene <orsoueneelaouizeb@gmail.com>
2025-02-15 18:22:54 +0100

    Aggiunto pagina lostPage

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import AddPropertyPage from "./pages/AddPropertyPage";
 import PropertyDetail from "./pages/PropertyDetailPage";
 import ErrorPage from "./pages/ErrorPage";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import LostPage from "./pages/lostPage";
 import { RefsProvider } from "./Context/RefsContext";
 import { lazy, Suspense } from "react";
 import FavouritesPage from "./pages/FavouritesPage";
@@ -16,25 +17,26 @@ function App() {
   return (
     <QueryClientProvider client={queryClient}>
       {/* <RefsProvider> */}
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" Component={DefaultLayout}>
-              <Route
-                index
-                element={
-                  <Suspense fallback={<>Suspense Loading...</>}>
-                    <LazyHomePage />
-                  </Suspense>
-                }
-              />
-              <Route path="search" Component={SearchPropertyPage} />
-              <Route path="addproperty" Component={AddPropertyPage} />
-              <Route path="detail/:id" Component={PropertyDetail} />
-              <Route path="favourites" Component={FavouritesPage} />
-              <Route path="*" Component={ErrorPage} />
-            </Route>
-          </Routes>
-        </BrowserRouter>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" Component={DefaultLayout}>
+            <Route
+              index
+              element={
+                <Suspense fallback={<>Suspense Loading...</>}>
+                  <LazyHomePage />
+                </Suspense>
+              }
+            />
+            <Route path="search" Component={SearchPropertyPage} />
+            <Route path="addproperty" Component={AddPropertyPage} />
+            <Route path="detail/:id" Component={PropertyDetail} />
+            <Route path="favourites" Component={FavouritesPage} />
+            <Route path="lost" Component={LostPage} />
+            <Route path="*" Component={ErrorPage} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
       {/* </RefsProvider> */}
     </QueryClientProvider>
   );
