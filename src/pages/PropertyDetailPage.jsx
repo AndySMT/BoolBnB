7760fcd69a58fc32b99bc1ecd32104f852c55e13
Orsouene <orsoueneelaouizeb@gmail.com>
2025-02-15 12:49:42 +0100

    Pagina propertyDetailPage : Scritto a mano una descrizione della città per le prime tre cards e una descrizione generale per qualsiasi città  .

--- src/pages/PropertyDetailPage.jsx
+++ src/pages/PropertyDetailPage.jsx
@@ -273,7 +273,7 @@ function SectionPosition({ property }) {
                         <div className="flex items-center flex-wrap gap-1 text-base sm:text-lg font-semibold">
                             <div className="flex items-center gap-1">
                                 <FaMapMarkerAlt />
-                                <span>Numero civico:</span>{" "}
+                                <span>Zip code:{property.zipcode}</span>
                             </div>
                             <span className="font-normal">{property.address_number}</span>
                         </div>
@@ -286,11 +286,18 @@ function SectionPosition({ property }) {
                         </div>
                     </div>
                     <div className="my-6">
-                        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quibusdam
-                        ipsa aspernatur corporis error, voluptates excepturi? Veniam
-                        reprehenderit provident voluptates animi.
+                        {property.city === "Roma" ? (
+                            "Elegante quartiere di Roma, molto strategico per la sua posizione, dove troverete negozi di ogni genere, supermercati, bar, tabaccherie, caffetterie e servizi di ristorazione da asporto e non."
+                        ) : property.city === "Milano" ? (
+                            "Milano è una delle città più dinamiche d'Italia, nota per la sua moda, arte e cultura. Il centro città è un mix affascinante di antico e moderno, con il famoso Duomo, gallerie d'arte e quartieri pieni di negozi di alta moda."
+                        ) : property.city === "Firenze" ? (
+                            "Firenze, culla del Rinascimento, è una città che incanta con le sue opere d'arte, i palazzi storici e la bellezza delle sue piazze. Qui potrai passeggiare lungo l'Arno, ammirare il Duomo e visitare i famosi musei come gli Uffizi."
+                        ) : (
+                            `${property.city} è una città vivace, ricca di storia, con strade affollate, edifici moderni, parchi verdi, cultura vibrante e diverse tradizioni`
+                        )}
                     </div>
                 </div>
+
                 {/* Mappa */}
                 <div className="lg:w-1/2 lg:py-8 lg:px-4">
                     <MyMapComponent property={property} />
