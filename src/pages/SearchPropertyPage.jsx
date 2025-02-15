6f59f24586222e237b11727de648773f0e207169
Aj-Herrera-99 <ajherrera999@gmail.com>
2025-02-15 08:20:16 +0100

    pt2

--- src/pages/SearchPropertyPage.jsx
+++ src/pages/SearchPropertyPage.jsx
@@ -13,12 +13,11 @@ const initialOptions = {
 };
 
 function SearchPropertyPage() {
-
     const [inputValue, setInputValue] = useState(""); // controllo dinamico dell'input della citta
     const [params, setParams] = useState({}); // per salvare l'oggetto params per la query in get
     const [isEnabled, setIsEnabled] = useState(false); // booleano che abilita o no il fetch (controllare useGetPropertiesQuery)
     const [optSelected, setOptSelected] = useState(initialOptions); // oggetto che salva le options
-    
+
     const { data, isLoading, isError, refetch } = useGetPropertiesQuery(
         params,
         isEnabled
@@ -78,9 +77,8 @@ function SearchPropertyPage() {
                             disabled={!isEnabled || !inputValue.length}
                             type="submit"
                             className={`${
-                                !isEnabled ||
-                                (!inputValue.length &&
-                                    "!cursor-not-allowed opacity-50")
+                                (!isEnabled || !inputValue.length) &&
+                                "!cursor-not-allowed opacity-50"
                             } px-4 py-2 bg-amber-500 rounded-lg cursor-pointer`}
                         >
                             Applica filtri
