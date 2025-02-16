bbeaf1a6b8d5f7059abc150c8451ed718d142b94
Aj-Herrera-99 <ajherrera999@gmail.com>
2025-02-16 17:53:38 +0100

    aggiornati server endpoints

--- src/globals/apiUrls.js
+++ src/globals/apiUrls.js
@@ -3,3 +3,4 @@ export const propsEndpoint = "/api/properties"
 export const revsEndpoint = "/api/reviews"
 export const imagesUrl = "http://localhost:3000/images";
 export const contactEndpoint = "/api/email/send";
+export const likesEndpoint = "/api/likes";
