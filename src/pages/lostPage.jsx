73fa32f5356c0de931a941e0ff6d1b3d15707fa8
Aj-Herrera-99 <ajherrera999@gmail.com>
2025-02-20 07:02:12 +0100

    minor changes

--- src/pages/lostPage.jsx
+++ src/pages/lostPage.jsx
@@ -36,7 +36,7 @@ function LostPage() {
                         </g>
                     </svg>
 
-                    <h1 className="mt-6 text-2xl font-bold tracking-tight text-gray-900 sm:text-4xl">Mi dispiace che sei capitato qui, ma sembra che abbiamo i server in manutenzione.!</h1>
+                    <h1 className="mt-6 text-2xl font-bold tracking-tight text-gray-900 sm:text-4xl">Mi dispiace che sei capitato qui, ma sembra che abbiamo i server in manutenzione!</h1>
 
                     <p className="mt-4 text-gray-500"> Potrai riprendere la ricerca della casa perfetta tra pochi minuti!</p>
                 </div>
